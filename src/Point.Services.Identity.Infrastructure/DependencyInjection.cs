12fd1fae262b40628c99c711532e0a6c9ae338b6
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2022-10-18 22:12:52 +0500

    added infrastructure DI

---
+++ src/Point.Services.Identity.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace Point.Services.Identity.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services)
+    {
+        return services;
+    }
+}
